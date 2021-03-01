5e7f4fe897142f9af5387b48f4bb4a6aa0e0de82
pierreguillaumelaurin <pierre-guillaumelaurin@Pierre-Guillaumes-MacBook-Pro.local>
2021-03-01 10:26:35 -0500

    Ajust spacing

--- app/routes/donate.js
+++ app/routes/donate.js
@@ -3,7 +3,7 @@ const config = require('config')
 module.exports = router => {
   router.get("/donate", async ctx => {
     ctx.state.model = {
-      title: "Donate",
+      title: "💰 Donate 💰",
       amount: ctx.query.amount || config.get("settings.defaultDonation")
     }
     await ctx.render('donate')
